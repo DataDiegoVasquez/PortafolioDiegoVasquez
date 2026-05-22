17ac10bb6a3359e746ce3bc9d6fdf1c93900b713
Diego_Vasquez <diegovasquez0197@gmail.com>
2026-05-22 15:02:30 -0500

    Modificacion en el perfil y logros haciendolos mas actuales y relevantes, ademas de agregar enlaces a redes sociales y al CV.

---
+++ src/data/achievements.ts
@@ -0,0 +1,47 @@
+export const ACHIEVEMENTS = [
+  {
+    entity: "FUNDACIÓN UNIVERSITARIA DEL AREA ANDINA",
+    name: "Profesional En Finanzas Y Negocios Internacionales",
+    date: "soon...",
+  },
+  {
+    entity: "GOOGLE",
+    name: "Google Cloud Certified - Associate Cloud Engineer",
+    date: "2023",
+    link: "https://google.accredible.com/dec669a1-1487-4de2-99f8-3c93420b6802"
+  },
+  {
+    entity: "CORRELATION ONE",
+    name: "Formación en Ciencia de Datos",
+    date: "2022",
+    link: "https://udistritaleduco-my.sharepoint.com/:b:/g/personal/dfvasquezr_udistrital_edu_co/EYDQbgx1II5Dj2rrw3NV6rkBo5WEzuI8LcjcM1qzqzw7yw?e=EByVnp"
+  },
+  {
+    entity: "UTP",
+    name: "Diplomado en Fundamentos de Programación en Python",
+    date: "2022",
+    link: "https://udistritaleduco-my.sharepoint.com/:b:/g/personal/dfvasquezr_udistrital_edu_co/Efc5upq5NuRIhvVD3JqtlPMBP-3dYHvTbmiW_qLfBkLMnA?e=0dFFEF"
+  },
+  {
+    entity: "UTP",
+    name: "Diplomado en Desarrollo de software",
+    date: "2022",
+    link: "https://udistritaleduco-my.sharepoint.com/:b:/g/personal/dfvasquezr_udistrital_edu_co/EcquVTkFSclCvZjG7s6w9WoBVi_TVbOZVjne0U-IU5kR5Q?e=AmLbBE"
+  },
+  {
+    entity: "UTP",
+    name: "Diplomado en Desarrollo de aplicaciones Web",
+    date: "2022",
+    link: "https://udistritaleduco-my.sharepoint.com/:b:/g/personal/dfvasquezr_udistrital_edu_co/EZnptxxPRCZLtXkNsjLjReABjNB27grPON0NJ6BPm2dUYA?e=jI6Tik"
+  },
+  {
+    entity: "SENA",
+    name: "Tecnólogo en Negociación Internacional",
+    date: "2022",
+  },
+  {
+    entity: "UNIVERSIDAD DISTRITAL",
+    name: "Profesional En Ingeniería Industrial",
+    date: "2020",
+  }
+];
