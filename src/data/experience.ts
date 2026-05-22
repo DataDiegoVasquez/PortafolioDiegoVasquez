4c668a9a0b173aedc2484297182f34a48bc14af5
Diego_Vasquez <diegovasquez0197@gmail.com>
2026-05-22 11:20:37 -0500

    Modificacion seccion Experiencia Laboral

---
+++ src/data/experience.ts
@@ -0,0 +1,27 @@
+export const EXPERIENCIE = [
+  {
+    date: "Diciembre 2022 - Actualidad",
+    title: "Ingeniero de datos | DATALYTICS COLOMBIA S.A.S.",
+    description: "Reduje el tiempo de procesamiento de datos mediante la optimización de flujos ETL en Databricks, consolidando un Datawarehouse escalable en GCP que da soporte a las decisiones gerenciales.",
+    tech: ["Databricks", "SnowFlake", "GCP", "Python", "SQL"],
+  },
+  {
+    date: "Noviembre 2021 – Septiembre 2022",
+    title: "Analista de datos Junior | LEGIS EDITORES S.A",
+    description: "Agilicé el análisis y visualización de datos mediante la creación de pipelines ETL automatizados en SQL y Python, centralizando el almacenamiento seguro en Google Cloud Platform.",
+    tech: ["Python", "SQL", "GCP", "ETL"],
+    link: "#Certificado",
+  },
+  {
+    date: "Febrero 2021 – Octubre 2021",
+    title: "Arquitecto de datos Junior | NEXA BPO",
+    description: "Diseñé estructuras y procesos eficientes para la gestión de datos, asegurando una alta disponibilidad y la integridad de la información para los equipos de desarrollo.",
+    tech: ["Bases de Datos", "Modelado de Datos"],
+  },
+  {
+    date: "Mayo 2020 – Octubre 2020",
+    title: "Prácticas de Ingeniería | PREVISORA SEGUROS S.A",
+    description: "Automaticé la extracción y agrupación de datos para la construcción de informes gerenciales periódicos, mejorando el control de la plataforma de Gestión Documental de la compañía.",
+    tech: ["Procesamiento de Datos", "Reporting"],
+  },
+];
